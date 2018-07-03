adfe83f87ff73eb10f4e198f1c20dc07c6474607
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2018-07-03 01:13:14 +0100

    Updated deck screen and deck to add description, updated readme

--- app/README.md
+++ app/README.md
@@ -1,25 +1,8 @@
-# NativeScript Angular Template
+# Voteshot :zap:
 
-This template creates a "Hello, world" NativeScript app using TypeScript and Angular.
+A simple mobile application, build with NativeScript + Angular (:heart:), to use while poker planning :coffee:.
 
-You can create a new app that uses this template with either the `--template` option.
-
-```
-tns create my-app-name --template tns-template-hello-world-ng
-```
-
-Or the `--ng` shorthand.
-
-```
-tns create my-app-name --ng
-```
-
-> Note: Both commands will create a new NativeScript app that uses the latest version of this template published to [npm] (https://www.npmjs.com/package/tns-template-hello-world-ng).
-
-If you want to create a new app that uses the source of the template from the `master` branch, you can execute the following:
-
-```
-tns create my-app-name --template https://github.com/NativeScript/template-hello-world-ng.git#master
-```
-
-**NB:** Please, have in mind that the master branch may refer to dependencies that are not on NPM yet!
+### Supports 
+1. Standard playing cards
+2. Fibonacci
+3. Other numerical sequence (0, ½, 1, 2, 3, 5, 8, 13, 21, 34, 100, bomb, question mark, coffee cup)
--- app/shared/decks.ts
+++ app/shared/decks.ts
@@ -4,46 +4,48 @@ export type Card = {
     value: string,
     icon?: string,
     useWebFont?: boolean,
-    unicode?: string
+    unicode?: string,
+    description?: string
 };
 
 export const Fibonacci: Cards = [
-    { id: 0, value: "0" },
-    { id: 1, value: "1" },
-    { id: 2, value: "2" },
-    { id: 3, value: "3" },
-    { id: 4, value: "5" },
-    { id: 5, value: "8" },
-    { id: 6, value: "13" },
-    { id: 7, value: "21" },
-    { id: 8, value: "34" },
-    { id: 9, value: "55" },
-    { id: 10, value: "89" },
-    { id: 11, value: "Bomb", icon: 'fa-bomb', useWebFont: true }
+    { id: 0, value: "0", description: "This task can be done instantly." },
+    { id: 1, value: "1", description: "This task needs one unit." },
+    { id: 2, value: "2", description: "This task needs two units." },
+    { id: 3, value: "3", description: "This task needs three units." },
+    { id: 4, value: "5", description: "This task needs five units." },
+    { id: 5, value: "8", description: "This task needs eight units." },
+    { id: 6, value: "13", description: "This task needs thirteen units." },
+    { id: 7, value: "21", description: "This task needs twenty one units." },
+    { id: 8, value: "34", description: "This task needs thirty four units." },
+    { id: 9, value: "55", description: "This task needs fifty five units." },
+    { id: 10, value: "89", description: "This task needs eighty nine units." },
+    { id: 11, value: "Bomb", icon: 'fa-bomb', useWebFont: true, description: "Cannot be completed." }
 ];
 
 export const MountainGoat: Cards = [
-    { id: 0, value: "0" },
-    { id: 1, value: "1/2" },
-    { id: 2, value: "1" },
-    { id: 3, value: "2" },
-    { id: 4, value: "3" },
-    { id: 5, value: "5" },
-    { id: 6, value: "8" },
-    { id: 7, value: "13" },
-    { id: 8, value: "21" },
-    { id: 9, value: "34" },
-    { id: 10, value: "100" },
-    { id: 11, value: "?", icon: 'fa-question', useWebFont: true },
-    { id: 12, value: "Coffee", icon: 'fa-coffee', useWebFont: true }
+    { id: 0, value: "0", description: "This task can be done instantly." },
+    { id: 1, value: "1/2", description: "This task is very easy." },
+    { id: 2, value: "1", description: "This task needs one unit." },
+    { id: 3, value: "2", description: "This task needs two units." },
+    { id: 4, value: "3", description: "This task needs three units." },
+    { id: 5, value: "5", description: "This task needs five units." },
+    { id: 6, value: "8", description: "This task needs eight units." },
+    { id: 7, value: "13", description: "This task needs thirteen units." },
+    { id: 8, value: "21", description: "This task needs twenty one units." },
+    { id: 9, value: "34", description: "This task needs thirty four units." },
+    { id: 10, value: "100", description: "This task needs one hundred units." },
+    { id: 11, value: "?", icon: 'fa-question', useWebFont: true, description: "Unsure." },
+    { id: 12, value: "Coffee", icon: 'fa-coffee', useWebFont: true, description: "I need a break." },
+    { id: 13, value: "Bomb", icon: 'fa-bomb', useWebFont: true, description: "Cannot be completed." }
 ];
 export const Standard: Cards = [
-    { id: 0, value: "Ace", unicode: "\u2660" },
-    { id: 1, value: "2" },
-    { id: 2, value: "3" },
-    { id: 3, value: "5" },
-    { id: 4, value: "8" },
-    { id: 5, value: "King", unicode: "\u265A" }
+    { id: 0, value: "Ace", unicode: "\u2660", description: "Easy task." },
+    { id: 1, value: "2", description: "This task needs two units." },
+    { id: 2, value: "3", description: "This task needs three units." },
+    { id: 3, value: "5", description: "This task needs five units." },
+    { id: 4, value: "8", description: "This task needs eight units." },
+    { id: 5, value: "King", unicode: "\u265A", description: "This item is too big to estimate." }
 ];
 
 export type Deck = {
